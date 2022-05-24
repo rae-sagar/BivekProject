5b2c96899d66dd6f629e6720d7b2591b4b4f78c4
Sagar <sagarchamling32@gmail.com>
2022-05-24 09:36:41 +0545

    Add project files.

---
+++ BivekProject/WebForm2.aspx
@@ -0,0 +1,204 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="BivekProject.WebForm2" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
+   <script src="notjs/jquery.table2excel.js"></script>
+
+    <style type="text/css">
+        #HeadingId, #searchID{
+            background-color:red;
+            color:white;          
+        }
+        .ReportKPIClass{
+            float:left;
+            margin-left:20px;
+        }
+        .tableClass{
+            margin-left:20px;
+            width:95%;
+        }
+        .downloadBtnClass{
+            background-color:transparent;
+            border-color:transparent;
+            float:right; 
+            margin-right:35px;
+            position:relative; 
+            top:50px;
+        }
+        .rowClass,.headingClass{
+            text-align:center;
+        }
+        .RedValidation:after {
+            content:" *";
+            color: red;
+         }
+        .RedValidation{
+            font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        }
+        .boxClass{
+            position:relative;
+            right:60px;
+        }
+        .labelClass{
+            color:firebrick;
+            font-style:italic;
+
+        }
+    </style>
+    <script type="text/javascript">
+
+        function Down_Excel(idN) {
+            debugger;
+  
+            var idName = idN;
+            $('#'+idN).table2excel();        
+        }
+         
+        function Valid_form() {
+            var firstDate = document.getElementById("txtBoxTwo").value;
+            var secondDate = document.getElementById("txtBoxThree").value;
+            if (firstDate == "") {
+                 $('#txtBoxTwo').css('border-color','red');
+                alert("Plese select the date.");               
+                return false;
+            }
+            if (secondDate == "") {
+                $('#txtBoxTwo').css('border-color','black');
+                $('#txtBoxThree').css('border-color','red');
+                alert("Please select the date.");               
+                return false;
+            }
+            return true;
+        }
+
+        $(document).ready(function () {          
+            //------------hide section---------
+            $('.hiddenHeader').hide();
+            $('.hiddenColumnId').hide();
+          
+            //--------------makeEditable------
+            $(document).on('click', '.row_data', function () {
+                $(this).closest('div').attr('contenteditable', 'true')
+                $(this).addClass('bg-warning').css('padding', '5px')
+                $(this).focus()
+            });
+
+            $(document).on('focusout', '.row_data', function () {
+                var row_div = $(this)
+                .removeAttr('contenteditable')
+                .removeClass('bg-warning')
+                .css('padding', '')
+            });         
+
+           $(".row_data").keypress(function (e) {
+              if(e.which == 46){
+                  if ($(this).val().indexOf('.') != -1) {
+                     alert("Number only.");
+                    return false;
+              }
+                 }
+
+               if (e.which != 8 && e.which != 0 && e.which != 46 && (e.which < 48 || e.which > 57)) {
+                   alert("Number only.");
+                  return false;
+              }
+            });
+
+           
+
+            $('#BEclass').attr('rowspan')
+
+            $('.UpdateClass').click(function () {
+                var currentRow = $(this).closest('tr');
+                var col1 = currentRow.find('td:eq(0)').text();
+                var col2 = currentRow.find('td:eq(7)').text();
+                var col3 = currentRow.find('td:eq(8)').text();
+                var col4 = currentRow.find('td:eq(9)').text();
+                var col5 = currentRow.find('td:eq(10)').text();
+                var col6 = currentRow.find('td:eq(11)').text();
+                var col7 = currentRow.find('td:eq(12)').text();                
+                
+                var jsondata = {
+                    Id: col1,
+                    Reg: col2,
+                    GmeLoan: col3,
+                    SimCard: col4,
+                    GmePass: col5,
+                    Issue: col6,
+                    Other: col7
+                };
+
+                     $.ajax({
+                    url: 'WebForm2.aspx/Up_data',
+                         type: 'Post',
+                         data: JSON.stringify(jsondata),
+                    contentType: 'application/json; charset=utf - 8',
+                    dataType: 'json',
+                    success: function (responce) {
+                        alert(responce.d);
+                    },      
+                    failure: function() {
+                         alert('Failed');
+                    }
+                    });          
+            });
+        });               
+    </script> 
+    <title></title>
+</head>
+<body>
+    <label style="background-color:red; color:white; margin-left:20px">KPI Report Entry</label>
+   <form id="form1" runat="server" class="col-md-12">
+         <div class="panel panel-default" style="width:50%; background-color:ghostwhite; padding-top:20px; margin-left:6px;">         
+            <div class="panel-body">
+                <div class="form-group">
+                    <asp:Label CssClass="col-md-4 RedValidation" Text="From Date:" runat="server" />
+                    <asp:TextBox ID="txtBoxTwo" CssClass="col-md-8 input-sm boxClass" TextMode="Date" runat="server" />               
+                </div>
+            </div>
+
+            <div class="panel-body">
+                <div class="form-group">
+                    <asp:Label CssClass="col-md-4 RedValidation" Text="To Date:" runat="server" />
+                    <asp:TextBox ID="txtBoxThree" CssClass=" col-md-8 input-sm boxClass" TextMode="Date" runat="server" />                   
+                </div>
+            </div>
+
+            <div class="panel-body">
+                <div class="form-group">
+                    <asp:Label CssClass="col-md-4 RedValidation" Text="Branch Name:" runat="server" />
+                    <asp:DropDownList ID="drpList" CssClass=" col-md-8 input-sm boxClass" runat="server">
+                        <asp:ListItem Text="All Branch" Value="0" />
+                        <asp:ListItem Text="Mobile Remit" Value="1" />
+                        <asp:ListItem Text="Songu-ri" Value="2" />
+                        <asp:ListItem Text="Hyehwa" Value="3" />
+                        <asp:ListItem Text="DDM CIS" Value="4" />
+                        <asp:ListItem Text="Mongol Town" Value="5" />
+                        <asp:ListItem Text="Gwangju" Value="6" />
+                        <asp:ListItem Text="Suwon" Value="7" />
+                        <asp:ListItem Text="GME Online" Value="8" />
+                        <asp:ListItem Text="Dongdaemun" Value="9" />
+                        <asp:ListItem Text="Ansan" Value="10" />
+                        <asp:ListItem Text="Hwaseong" Value="11" />
+                        <asp:ListItem Text="Gimhae" Value="12" />
+                    </asp:DropDownList>                   
+                </div>
+            </div>
+            
+            <div class="panel-body">
+                <asp:Button Text="Search" ID="searchID" CssClass="btn col-md-2 RedValidation" runat="server" OnClientClick="return Valid_form()" OnClick="Search_Click"/>              
+            </div>
+        </div>  
+    </form>
+
+     <asp:PlaceHolder ID="placeHolderId" runat="server"></asp:PlaceHolder>
+</body>
+</html>
+</asp:Content>
---
+++ BivekProject/WebForm2.aspx.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Text;
+
+namespace BivekProject
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        StringBuilder table = new StringBuilder();
+
+        string strcon = ConfigurationManager.ConnectionStrings["HTMLDB"].ConnectionString;
+        void Load_data()
+        {
+
+            SqlConnection con = new SqlConnection(strcon);
+            con.Open();
+
+            DateTime StartDate = Convert.ToDateTime(txtBoxTwo.Text);
+            DateTime EndDate = Convert.ToDateTime(txtBoxThree.Text);
+            if (drpList.SelectedValue == "0")
+            {
+                int[] droplistNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+                string[] dropListNames = { "random", "Mobile Remit", "Songu-ri", "Hyehwa", "DDM CIS", "Mongol Town", "Gwangju", "Suwon", "GME Online", "Dongdaemun", "Ansan", "Hwaseong", "Gimhae" };
+
+                foreach (int i in droplistNumbers)
+                {
+                    int l = 0;
+
+                    foreach (DateTime day in EachCalendarDay(StartDate, EndDate))
+                    {
+                        placeHolderId.Controls.Clear();
+
+                        SqlCommand comm = new SqlCommand("HTMLTableSP", con);
+                        comm.CommandType = CommandType.StoredProcedure;
+                        comm.Parameters.AddWithValue("@Flag", "Show");
+                        comm.Parameters.AddWithValue("@TableValue", i);
+                        comm.Parameters.AddWithValue("@DateSelected", string.Format("{0:yyyy/MM/dd}", day.ToString()));
+                        comm.ExecuteNonQuery();
+
+                        var forDate = day.ToString();
+
+                        SqlDataReader rd = comm.ExecuteReader();
+
+                        int idForTable = 1;
+                        // ---------- Report Details -------------------- 
+                        table.Append("<div class='ReportKPIClass'><label>Branch KPI Reports</label><br>");
+                        table.Append("<label>From Date:<span class='labelClass'>'" + day.ToShortDateString() + "'</span></label> &nbsp<label>To Date: <span class='labelClass'>'" + day.ToShortDateString() + "'</span></label><br>");
+                        table.Append("<label>Branch: <span class='labelClass'>'" + dropListNames[i] + "'</span></label></div>");
+
+                        // ---------- Excel Download Button Section ----------- 
+                        table.Append("<div style='position: relative;'><button onClick='Down_Excel(\"" + dropListNames[i].Replace(' ', 'a') + (i + l) + "\")' class='downloadBtnClass' id='" + dropListNames[i] + forDate + "'>");
+                        table.Append("<img src='/excel img/excel2.jpg' height='20'/></button></div>");
+
+                        // ----------- Table Heading --------- 
+                        table.Append("<table border='1' class='table table-hover tableClass' id='" + dropListNames[i].Replace(' ', 'a') + (i + l) + "'>");
+                        table.Append("<tr id='HeadingId'><th class='hiddenHeader'>HiddenId</th><th class='hiddenHeader'>BranchId</th><th class='hiddenHeader'>HiddenDate</th>");
+                        table.Append("<th class='headingClass'>Sn</th><th class='headingClass'>Username</th><th class='headingClass'>Name</th><th class='headingClass'>Nationality</th>");
+                        table.Append("<th class='headingClass'>Registration</th><th class='headingClass'>GMELoan</th><th class='headingClass'>SimCard</th><th class='headingClass'>GMEPass</th><th class='headingClass'>IssueSolved</th>");
+                        table.Append("<th class='headingClass'>Other(Share,Bond,TaxRefund)</th><th class='headingClass'>StaffEfficiency</th><th class='headingClass'>BranchEfficiency</th><th class='headingClass'>Edit</th></tr>");
+
+                        if (rd.HasRows)
+                        {
+                            while (rd.Read())
+                            {
+                                table.Append("<tr class='rowClass'>");
+                                table.Append("<td class='hiddenColumnId'>" + rd[0] + "</td>");
+                                table.Append("<td class='hiddenColumnId'>" + rd[1] + "</td>");
+                                table.Append("<td class='hiddenColumnId'>" + rd[2] + "</td>");
+                                table.Append("<td id='rowId'>" + idForTable + "</td>");
+                                table.Append("<td>" + rd[3] + "</td>");
+                                table.Append("<td>" + rd[4] + "</td>");
+                                table.Append("<td>" + rd[5] + "</td>");
+                                table.Append("<td><div class='row_data' id='RegId'>" + rd[6] + "</div></td>");
+                                table.Append("<td><div class='row_data' id='GmeLoanId'>" + rd[7] + "</div></td>");
+                                table.Append("<td><div class='row_data' id='SimCardId'>" + rd[8] + "</div></td>");
+                                table.Append("<td><div class='row_data' id='GmePassId'>" + rd[9] + "</div></td>");
+                                table.Append("<td><div class='row_data' id='IssueId'>" + rd[10] + "</div></td>");
+                                table.Append("<td><div class='row_data' id='OtherId'>" + rd[11] + "</div></td>");
+                                table.Append("<td>" + rd[12] + "</td>");
+                                table.Append("<td><div id='BEclass'>" + rd[13] + "</div></td>");
+                                table.Append("<td><div><input type='button' value='Update' class='btn btn-danger UpdateClass'/></div></td>");
+                                table.Append("</tr>");
+
+                                idForTable++;
+                            }
+                        }
+                        else
+                        {
+                            table.Append("<tr><td colspan='13' style='text-align:center;font-weight: bold;'>No data found.</td></tr>");
+                        }
+                        l++;
+                        table.Append("</table>");
+                        placeHolderId.Controls.Add(new Literal { Text = table.ToString() });
+                        rd.Close();
+                    }
+                }
+            }
+            else
+            {
+                foreach (DateTime day in EachCalendarDay(StartDate, EndDate))
+                {
+                    var forDate = day.ToShortDateString();
+                    placeHolderId.Controls.Clear();
+
+                    SqlCommand comm = new SqlCommand("HTMLTableSP", con);
+                    comm.CommandType = CommandType.StoredProcedure;
+                    comm.Parameters.AddWithValue("@Flag", "Show");
+                    comm.Parameters.AddWithValue("@TableValue", drpList.SelectedValue);
+                    comm.Parameters.AddWithValue("@DateSelected", string.Format("{0:yyyy/MM/dd}", day.ToString()));
+                    comm.ExecuteNonQuery();
+
+
+                    SqlDataReader rd = comm.ExecuteReader();
+
+                    int idForTable = 1;
+                    //------------------ Report Details --------------------
+                    table.Append("<div class='ReportKPIClass'><label>Branch KPI Reports</label><br>");
+                    table.Append("<label>From Date:<span class='labelClass'> '" + day.ToShortDateString() + "'</span></label> &nbsp<label>To Date:<span class='labelClass'> '" + day.ToShortDateString() + "'</span></label><br>");
+                    table.Append("<label>Branch:<span class='labelClass'> '" + drpList.SelectedItem.Text + "'</span></label></div>");
+
+                    // ----------------- Excel Download Button Section -----------------------
+                    table.Append("<div style='position: relative;'><button id='" + forDate + "'  onClick='Down_Excel(\"" + forDate.Replace('/', 'a') + "\")' class='downloadBtnClass'>");
+                    table.Append("<img src='/excel img/excel2.jpg' height='20'/></button></div>");
+
+                    // ----------------- Table Heading ---------------------------------------
+                    table.Append("<table border='1' class='table table-hover tableClass' id='" + forDate.Replace('/', 'a') + "' style='margin-left:20px; width:95%'>");
+                    table.Append("<tr id='HeadingId'><th class='hiddenHeader'>HiddenId</th><th class='hiddenHeader'>BranchId</th><th class='hiddenHeader'>HiddenDate</th>");
+                    table.Append("<th class='headingClass'>Sn</th><th class='headingClass'>Username</th><th class='headingClass'>Name</th><th class='headingClass'>Nationality</th>");
+                    table.Append("<th class='headingClass'>Registration</th><th class='headingClass'>GMELoan</th><th class='headingClass'>SimCard</th><th class='headingClass'>GMEPass</th><th class='headingClass'>IssueSolved</th>");
+                    table.Append("<th class='headingClass'>Other(Share,Bond,TaxRefund)</th><th class='headingClass'>StaffEfficiency</th><th class='headingClass'>BranchEfficiency</th><th class='headingClass'>Edit</th></tr>");
+
+                    if (rd.HasRows)
+                    {
+                        while (rd.Read())
+                        {
+                            table.Append("<tr class='rowClass'>");
+                            table.Append("<td class='hiddenColumnId'>" + rd[0] + "</td>");
+                            table.Append("<td class='hiddenColumnId'>" + rd[1] + "</td>");
+                            table.Append("<td class='hiddenColumnId'>" + rd[2] + "</td>");
+                            table.Append("<td id='rowId'>" + idForTable + "</td>");
+                            table.Append("<td>" + rd[3] + "</td>");
+                            table.Append("<td>" + rd[4] + "</td>");
+                            table.Append("<td>" + rd[5] + "</td>");
+                            table.Append("<td><div class='row_data' id='RegId'>" + rd[6] + "</div></td>");
+                            table.Append("<td><div class='row_data' id='GmeLoanId'>" + rd[7] + "</div></td>");
+                            table.Append("<td><div class='row_data' id='SimCardId'>" + rd[8] + "</div></td>");
+                            table.Append("<td><div class='row_data' id='GmePassId'>" + rd[9] + "</div></td>");
+                            table.Append("<td><div class='row_data' id='IssueId'>" + rd[10] + "</div></td>");
+                            table.Append("<td><div class='row_data' id='OtherId'>" + rd[11] + "</div></td>");
+                            table.Append("<td>" + rd[12] + "</td>");
+                            table.Append("<td>" + rd[13] + "</td>");
+                            table.Append("<td><div><input type='button' value='Update' class='btn btn-danger UpdateClass'/></div></td>");
+                            table.Append("</tr>");
+
+                            idForTable++;
+                        }
+                    }
+                    else
+                    {
+                        table.Append("<tr><td colspan='13' style='text-align:center;font-weight: bold;'>No data found.</td></tr>");
+                    }
+                    table.Append("</table>");
+                    placeHolderId.Controls.Add(new Literal { Text = table.ToString() });
+                    rd.Close();
+                }
+                con.Close();
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Search_Click(object sender, EventArgs e)
+        {
+            Load_data();
+
+        }
+        public static IEnumerable<DateTime> EachCalendarDay(DateTime startDate, DateTime endDate)
+        {
+            for (var date = startDate.Date; date.Date <= endDate.Date; date = date.AddDays(1)) yield
+            return date;
+        }
+
+        [System.Web.Services.WebMethod]
+
+        public static string Up_data(int Id, int Reg, int GmeLoan, int SimCard, int GmePass, int Issue, int Other)
+        {
+            string strcon = ConfigurationManager.ConnectionStrings["HTMLDB"].ConnectionString;
+            SqlConnection con = new SqlConnection(strcon);
+            con.Open();
+            SqlCommand comm = new SqlCommand("HTMLTableSP", con);
+            comm.CommandType = CommandType.StoredProcedure;
+            comm.Parameters.AddWithValue("@Flag", "Update");
+            comm.Parameters.AddWithValue("@Id", Id);
+            comm.Parameters.AddWithValue("@Registration", Reg);
+            comm.Parameters.AddWithValue("@GMELoan", GmeLoan);
+            comm.Parameters.AddWithValue("@SimCard", SimCard);
+            comm.Parameters.AddWithValue("@GmePass", GmePass);
+            comm.Parameters.AddWithValue("@IssueSolved", Issue);
+            comm.Parameters.AddWithValue("@Other", Other);
+
+
+
+            SqlDataReader res = comm.ExecuteReader();
+
+            if (res.HasRows)
+            {
+                while (res.Read())
+                {
+                    int errorCode = Convert.ToInt32(res[0]);
+                    //string msg = Convert.ToString(res[1]);
+
+                    if (errorCode == 0)
+                    {
+                        return "Successfully Updated.";
+                    }
+                    else
+                    {
+                        return "Error.";
+                    }
+                }
+            }
+
+            con.Close();
+            return "Error.";
+
+            //var thisPage = new FinalTable();
+            //thisPage.Load_data();
+
+            //FinalTable obj = new FinalTable();
+            //obj.Load_data();
+        }
+    }
+}
+    
